cebaece74e03103e441bbe6c1f438956313beb70
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2024-12-17 14:53:37 +0100

    fix dep

--- app/src/main/java/com/destructo/botox/UserIDActivity.kt
+++ app/src/main/java/com/destructo/botox/UserIDActivity.kt
@@ -8,23 +8,19 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.google.android.gms.tasks.OnCompleteListener
+
 class UserIDActivity : AppCompatActivity() {
 
     //private lateinit var tvSubscriptionCode: TextView
