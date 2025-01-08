b21f6d8ba266e4ce78b1c10ab01862412fb38afe
Khalifa RHARBAOUI RASSAME <khalifarsm@outlook.com>
2025-01-08 22:47:08 +0100

    upload keysote

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore-botox.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
